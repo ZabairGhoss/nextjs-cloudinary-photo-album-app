c48bccaa3cd06635ad2b8e9310466567ca194915
Zabair Ghoss <czubair307@gmail.com>
2023-10-06 21:52:51 +0500

    UserNavbar configured with Shadcn UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import UserNavbar from '@/components/UserNavbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='dark'>
+      <body className={inter.className}>
+        <UserNavbar />
+        {children}
+        </body>
     </html>
   )
 }
---
+++ src/components/UserNavbar.tsx
@@ -0,0 +1,20 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import React from "react";
+
+const UserNavbar = () => {
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4  container mx-auto">
+        PHOTOS APP
+        <div className="ml-auto flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserNavbar;
