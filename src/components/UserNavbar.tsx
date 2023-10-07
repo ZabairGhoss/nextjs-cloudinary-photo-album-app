f0cd8d6a0f699722b2ca7f6949cc41d22129af16
Zabair Ghoss <czubair307@gmail.com>
2023-10-07 17:31:27 +0500

    configured Gallery Component into the app

--- src/components/UserNavbar.tsx
+++ src/components/UserNavbar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const UserNavbar = () => {
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4 container mx-auto">
+      <div className="flex h-16 items-center px-4 container mx-auto font-bold">
         PHOTOS APP
         <div className="ml-auto flex items-center space-x-4">
           <Avatar>
