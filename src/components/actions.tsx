576875afa88ea43ec20233cf59f508be55117322
Zabair Ghoss <czubair307@gmail.com>
2023-10-10 12:08:17 +0500

    component/Gallery - Actions to set new albums by user embedded

--- src/components/actions.tsx
+++ src/components/actions.tsx
@@ -1,6 +1,9 @@
-"use server"
+"use server";
+import { SearchResult } from "@/app/gallery/page";
 import cloudinary from "cloudinary";
 
-export async function CreateFolder(folder: string) {
-    const existingFolder = await cloudinary.v2.api.create_folder(folder);
+export async function AddImageToAlbum(image: SearchResult, album: string) {
+  await cloudinary.v2.api.create_folder(album);
+
+  cloudinary.v2.uploader.rename(image.public_id, `${album}/${image.public_id}`);
 }
