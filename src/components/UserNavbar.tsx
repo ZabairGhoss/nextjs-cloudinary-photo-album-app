23c1484e8be3c7ec99aac862c25c3b6f1c2ab6b0
Zabair Ghoss <czubair307@gmail.com>
2023-10-13 00:30:37 +0500

    Revert "component/UserNavbar - App logo and home link implemented"

--- src/components/UserNavbar.tsx
+++ src/components/UserNavbar.tsx
@@ -1,23 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 const UserNavbar = () => {
   return (
     <div className="border-b">
-      <div className="flex h-16 items-center px-4  container mx-auto">
-        <Link href={"/"}>
-        <div className="flex items-center">
-        <Image
-          src="/album.png"
-          width="50"
-          height="50"
-          alt="ZG photo album app logo"
-        />
-        ZG PHOTOS ALBUM
-        </div>
-        </Link>
       <div className="flex h-16 items-center px-4 container mx-auto font-bold">
         PHOTOS APP
         <div className="ml-auto flex items-center space-x-4">
