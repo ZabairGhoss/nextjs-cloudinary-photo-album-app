ca94081169fdf332fa5d494a3c1f6c12d19097b9
Zabair Ghoss <czubair307@gmail.com>
2023-10-16 22:07:05 +0500

    UploadResult type added in Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,20 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+type UploadResult = {
+  info: {
+    public_id: string;
+  };
+  event: "success";
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1>Welcome to Next-Cloudinary Photo Album Web App</h1>
-        <Button className="mt-10">
-          <Link href={'/gallery'}>
-            Open Gallery
-          </Link>
-        </Button>
-    </main> 
+      <Button className="mt-10">
+        <Link href={"/gallery"}>Open Gallery</Link>
+      </Button>
+    </main>
   );
 }
